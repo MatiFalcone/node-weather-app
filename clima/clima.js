fe0a2fd73a946ecb67bfbd3e4fff9c1944a5ab89
matifalcone <matias.falcone@gmail.com>
2020-08-26 21:14:34 +1000

    Secreted API access keys

--- clima/clima.js
+++ clima/clima.js
@@ -5,7 +5,7 @@ const getClima = async(latitud, longitud) => {
 // Codifico el valor recibido por consola
 	let encodedLatitud = encodeURI(latitud);
 	let encodedLongitud = encodeURI(longitud);
-	let accessKey = '59d6faf224408910d8cebc31d9c9c2c9';
+	let accessKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
 // Realizo el GET a la API
 	const respuesta = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ encodedLatitud }&lon=${ encodedLongitud }&appid=${ accessKey }&units=metric`);
